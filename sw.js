2f56bebedf9fb833b8bb6f9f7e312019d74a15b1
sh2132-sketch <sh2132@sussex.ac.uk>
2026-07-27 16:14:10 +0800

    update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'workbench-v1';
+const CACHE = 'workbench-v2';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', e => {
